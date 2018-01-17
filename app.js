d84583dee40a716f74de2bb79f80a95a8919ca60
Diondre Edwards <diondre27@hotmail.com>
2018-01-17 19:00:35 -0400

    Initial view changes

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
+const exphbs = require('express-handlebars');
+const helpers = require('handlebars-helpers');
+
+const comparison = helpers.comparison();
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
@@ -62,7 +66,8 @@ mongoose.connection.on('error', (err) => {
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: comparison, partialsDir: ['views/partials'] }));
+app.set('view engine', 'handlebars');
 app.use(expressStatusMonitor());
 app.use(compression());
 app.use(sass({
